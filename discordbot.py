b1a7d9951de7b6626f256f5c2b469aae2124f0f5
AoiDeveloper <76095792+AoiDeveloper@users.noreply.github.com>
2020-12-16 17:47:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,6 +5,10 @@ token = os.environ['bot_token']
 
 client = discord.Client()
 
+# ここからプログラム
+prefix = '_'
+casting = []
+
 @client.event
 async def on_ready():
     print('**---起動しました--**')
@@ -13,7 +17,12 @@ async def on_ready():
 async def on_message(message):
     if message.author.bot:
         return
-    if message.content == '_test':
-        await message.channel.send('**---Botは正常にインストールされています---**')
-
+    if message.content[0] == prefix:
+        cmd = message.content.split()[0][1:]
+        args = message.content.split()[1:]
+        if cmd == 'test':
+            await message.channel.send('**---Botは正常にインストールされています---**')
+        elif cmd == 'prefix':
+            prefix = args[0]
+        
 client.run(token)
