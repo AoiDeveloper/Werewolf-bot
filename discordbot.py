904e31be457a570dd332a9a94fc0f3ab8e6068ef
AoiDeveloper <76095792+AoiDeveloper@users.noreply.github.com>
2020-12-16 17:32:56 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,19 @@
-from discord.ext import commands
+import discord
 import os
-import traceback
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+token = os.environ['bot_token']
 
+client = discord.Client()
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+@client.event
+async def on_ready():
+    print('**---起動しました--**')
 
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if message.content == '_test':
+        await message.channel.send('**---Botは正常にインストールされています---**')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
-bot.run(token)
+client.run(token)
