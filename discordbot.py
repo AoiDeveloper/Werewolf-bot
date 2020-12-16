cf2049c56adb0bc3af1b72813918467bfb5d7501
AoiDeveloper <76095792+AoiDeveloper@users.noreply.github.com>
2020-12-16 18:32:39 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -19,5 +19,7 @@ async def on_message(message):
     args = message.content.split()[1:]
     if cmd == 'test':
         await message.channel.send('**---Botは正常にインストールされています---**')
-
+    if cmd == 'prefix':
+        prefix = args[0]
+        await message.channel.send(f'**コマンドプレフィックスを{prefix}に設定しました**')
 client.run(token)
